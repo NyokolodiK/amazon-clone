040e33b2f9f0d897b875d1e6755eaa31706817e6
Kagiso <kagiso@bazaruto.com>
2024-06-11 18:11:57 +0200

    add place order form

---
+++ app/api/auth/orders/route.ts
@@ -0,0 +1,62 @@
+import { auth } from "@/lib/auth";
+import dbConnect from "@/lib/dbConnect";
+import OrderModel, { OrderItem } from "@/lib/models/OrderModel";
+import ProductModel from "@/lib/models/ProductModel";
+import { calcPrices, round2 } from "@/lib/utils";
+
+export const POST = auth(async (req: any) => {
+  if (!req.auth) {
+    return Response.json(
+      { message: "unauthorized" },
+      {
+        status: 401,
+      }
+    );
+  }
+  const { user } = req.auth;
+  try {
+    const payload = await req.json();
+    await dbConnect();
+    const dbProductPrices = await ProductModel.find(
+      {
+        _id: { $in: payload.items.map((x: { _id: string }) => x._id) },
+      },
+      "price"
+    );
+    const dbOrderItems = payload.items.map((x: { _id: string }) => ({
+      ...x,
+      product: x._id,
+      price: dbProductPrices.find((x) => x._id === x._id).price,
+      _id: undefined,
+    }));
+
+    const { itemsPrice, taxPrice, shippingPrice, totalPrice } =
+      calcPrices(dbOrderItems);
+
+    const newOrder = new OrderModel({
+      items: dbOrderItems,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+      shippingAddress: payload.shippingAddress,
+      paymentMethod: payload.paymentMethod,
+      user: user._id,
+    });
+
+    const createdOrder = await newOrder.save();
+    return Response.json(
+      { message: "Order has been created", order: createdOrder },
+      {
+        status: 201,
+      }
+    );
+  } catch (err: any) {
+    return Response.json(
+      { message: err.message },
+      {
+        status: 500,
+      }
+    );
+  }
+}) as any;
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,23 @@
+import { OrderItem } from "./models/OrderModel";
+
 export const round2 = (num: number) =>
   Math.round(num * 100 + Number.EPSILON) / 100;
 
 export const convertDocToObj = (doc: any) => {
   doc._id = doc._id.toString();
   return doc;
+};
+
+export const calcPrices = (orderItems: OrderItem[]) => {
+  // Calculate the items price
+  const itemsPrice = round2(
+    orderItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+  );
+  // Calculate the shipping price
+  const shippingPrice = round2(itemsPrice > 100 ? 0 : 10);
+  // Calculate the tax price
+  const taxPrice = round2(Number((0.15 * itemsPrice).toFixed(2)));
+  // Calculate the total price
+  const totalPrice = round2(itemsPrice + shippingPrice + taxPrice);
+  return { itemsPrice, shippingPrice, taxPrice, totalPrice };
 };
